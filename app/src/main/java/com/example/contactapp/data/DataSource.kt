37b7c97d710642b132d52011bdf4d3e737d0279a
WinterWood0118 <dltjdals125@gmail.com>
2024-04-22 17:43:51 +0900

    싱글톤 디자인 변경

--- app/src/main/java/com/example/contactapp/data/DataSource.kt
+++ app/src/main/java/com/example/contactapp/data/DataSource.kt
@@ -6,17 +6,21 @@ import com.example.contactapp.function.getContacts
 class DataSource {
     companion object{
         private var INSTANCE: DataSource? = null
-        fun getInstance(): DataSource{
+        private lateinit var context: Context
+        fun getInstance(inputContext: Context): DataSource{
             return synchronized(DataSource::class.java){
                 val newInstance: DataSource = INSTANCE?: DataSource()
+                context = inputContext
                 INSTANCE = newInstance
                 newInstance
             }
         }
     }
+    var itemList = listOf<ContactInformation>()
 
-
-    fun getContactList(context: Context) = getContacts(context)
+    fun getContactList() {
+        itemList = getContacts(context)
+    }
 
     fun addContact(contactInformation: ContactInformation){
         contactList.add(contactInformation)
