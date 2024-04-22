1af908c35922739c857d96435ed416d9f59e0c70
WinterWood0118 <dltjdals125@gmail.com>
2024-04-22 19:16:42 +0900

    싱글톤 디자인 오류슈정

--- app/src/main/java/com/example/contactapp/data/DataSource.kt
+++ app/src/main/java/com/example/contactapp/data/DataSource.kt
@@ -6,31 +6,26 @@ import com.example.contactapp.function.getContacts
 class DataSource {
     companion object{
         private var INSTANCE: DataSource? = null
-        private lateinit var context: Context
-        fun getInstance(inputContext: Context): DataSource{
-            return synchronized(DataSource::class.java){
-                val newInstance: DataSource = INSTANCE?: DataSource()
-                context = inputContext
-                INSTANCE = newInstance
-                newInstance
-            }
+        fun getInstance(): DataSource{
+            return INSTANCE?: DataSource().apply { INSTANCE = this }
         }
-    }
-    var itemList = listOf<ContactInformation>()
 
-    fun getContactList() {
-        itemList = getContacts(context)
+    }
+    var itemList = mutableListOf<ContactInformation>()
+
+    fun getContactList(context: Context) {
+        itemList = getContacts(context).toMutableList()
     }
 
     fun addContact(contactInformation: ContactInformation){
-        contactList.add(contactInformation)
+        itemList.add(contactInformation)
     }
 
     fun deleteContact(contactInformation: ContactInformation){
-        contactList.remove(contactInformation)
+        itemList.remove(contactInformation)
     }
 
     fun switchLike(position: Int) {
-        contactList[position].isLike = !contactList[position].isLike
+        itemList[position].isLike = !contactList[position].isLike
     }
 }
