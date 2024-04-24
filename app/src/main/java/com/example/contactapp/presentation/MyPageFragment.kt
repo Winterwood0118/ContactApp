02da38ab5a656048f7cf6d7a04d03616bdf0fcd4
WinterWood0118 <dltjdals125@gmail.com>
2024-04-24 12:29:11 +0900

    mypage binding 작업 완료

--- app/src/main/java/com/example/contactapp/presentation/MyPageFragment.kt
+++ app/src/main/java/com/example/contactapp/presentation/MyPageFragment.kt
@@ -5,19 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.marginTop
-import com.example.contactapp.R
 import com.example.contactapp.data.ContactInformation
 import com.example.contactapp.databinding.FragmentMyPageBinding
 import com.example.contactapp.function.setBitmapProfile
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class MyPageFragment : Fragment() {
     private lateinit var binding: FragmentMyPageBinding
-    private var param1: String? = null
-    private var param2: String? = null
 
     private val myContact = ContactInformation(
         name = "홍길동",
@@ -28,10 +21,6 @@ class MyPageFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -60,12 +49,6 @@ class MyPageFragment : Fragment() {
     companion object {
 
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = MyPageFragment()
     }
 }
