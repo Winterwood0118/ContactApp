31e264c0421ade4dcaa18473d3e8b307474e3df1
Combro-Kim <rlawog@nate.com>
2024-04-25 16:55:27 +0900

    0425

--- app/src/main/java/com/example/contactapp/presentation/ContactDetailFragment.kt
+++ app/src/main/java/com/example/contactapp/presentation/ContactDetailFragment.kt
@@ -9,6 +9,7 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.CallLog
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -20,25 +21,24 @@ import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.os.BundleCompat.getParcelable
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.contactapp.R
+import com.example.contactapp.data.CallInformation
 import com.example.contactapp.data.ContactInformation
 import com.example.contactapp.databinding.FragmentContactDetailBinding
 import com.example.contactapp.function.setBitmapProfile
+import com.example.contactapp.presentation.call_log.CallLogAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class ContactDetailFragment : Fragment(), AddContact.ContactDetailUpdateListener {
-    private var selectedUri: Uri? = null
-    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-        if (uri != null) { //이미지를 선택할 경우
-            selectedUri = uri
-            //uri -> bitMap으로 변경
-            val imageBitmap = uriToBitmap(requireContext(), uri)
-            binding.ivProfile.setImageBitmap(imageBitmap)
-        } else {
-            Log.d("PhotoPicker", "No media selected") //todo 이미지 선택하지 않을 경우(알림추가?)
-        }
-    }
-
+    private lateinit var callLogAdapter: CallLogAdapter
 
     companion object {
         //요청 코드
@@ -71,9 +71,8 @@ class ContactDetailFragment : Fragment(), AddContact.ContactDetailUpdateListener
         (activity as? MainActivity)?.hideTabLayout() //TabLayout 숨기기
         setUpCall()
         setUpMessage()
-        setUpProfile()
+//        setUpProfile()
         changeData()
-
         initData()
 
         binding.ivBack.setOnClickListener {
@@ -82,12 +81,27 @@ class ContactDetailFragment : Fragment(), AddContact.ContactDetailUpdateListener
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             goBack()
         }
+
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        callLogAdapter = CallLogAdapter(emptyList()) // 초기화 시 빈 리스트 전달
+        //이유? 데이터를 저장하는 것이 아닌, 해당 번호와 일치하는 전화기록을 해당 Fragment가 열리면 가져올 것이기 때문에 emptyList() 사용
+        binding.recyclerView.adapter = callLogAdapter
+
+        val phoneNumber = "01012345678"
+        //DetailFragment에서 전화를 걸었을 때 실시간으로 RecyclerView가 변경되기 위한 코루틴 - CoroutineScope 생성
+        lifecycleScope.launch {//lifecycle 사용하면 생명주기를 인식하는 코루틴 생성 가능
+            while (true) {
+                val callRecords = getCallLogByPhoneNumber(phoneNumber) // <<일치하는 번호 input하기
+                callLogAdapter.callLog = callRecords
+                callLogAdapter.notifyItemChanged(0)
+            }
+        }
     }
 
     //전화
     private fun setUpCall() {
         binding.ivCall.setOnClickListener {
-            binding.tvNumber.text = "01012345678" //임시 데이터
             val phoneNumber = binding.tvNumber.text
             val callIntent = Uri.parse("tel:$phoneNumber")
             // 권한 확인
@@ -106,7 +120,6 @@ class ContactDetailFragment : Fragment(), AddContact.ContactDetailUpdateListener
     //문자
     private fun setUpMessage() {
         binding.ivMessage.setOnClickListener {
-            binding.tvNumber.text = "01012345678" //임시 데이터
             val phoneNumber = binding.tvNumber.text
             val sendUriSwipedPerson = Uri.parse("smsto:$phoneNumber")
             if (ContextCompat.checkSelfPermission(
@@ -122,7 +135,7 @@ class ContactDetailFragment : Fragment(), AddContact.ContactDetailUpdateListener
     }
 
     //photo picker 사용해서 이미지 선택
-    private fun setUpProfile() {
+/*    private fun setUpProfile() {
         binding.ivAdd.setOnClickListener {
             pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
@@ -130,7 +143,7 @@ class ContactDetailFragment : Fragment(), AddContact.ContactDetailUpdateListener
             selectedUri = null
             binding.ivProfile.setImageResource(R.drawable.ic_default_user)
         }
-    }
+    }*/
 
     //Detail 각 위젯에 데이터 띄우기 위함
     private fun inputEachData(data: ContactInformation) {
@@ -222,14 +235,63 @@ class ContactDetailFragment : Fragment(), AddContact.ContactDetailUpdateListener
             parentFragmentManager.setFragmentResult("updateData", bundle)
         }
     }
-    private fun uriToBitmap(context: Context, uri: Uri): Bitmap? {
-        return try {
-            val inputStream = context.contentResolver.openInputStream(uri)
-            BitmapFactory.decodeStream(inputStream)
-        } catch (e: IOException) {
-            e.printStackTrace()
-            null
+
+    //전화 기록 가져오기
+    private suspend fun getCallLogByPhoneNumber(phoneNumber: String): List<CallInformation> {
+        //suspend ? (코루틴을 사용하기 위해 사용)
+        //코루틴 호출 시 일시 중단될 수 있다'를 알림
+        val callRecords = mutableListOf<CallInformation>()
+
+        //전화 기록에 전화번호와 일치하는 DB 가져오기
+        withContext(Dispatchers.IO) { //백그라운드 작업 처리 위함
+            //Cursor c = cr.query(CallLog.Calls.CONTENT_URI, null, null, null, null); 참조
+            //Cursor c = cr.query(Selection, Selection Arguments, Group By, Having, Order By); 참조
+            val cursor = context?.contentResolver?.query( //sql query: 데이터 검색을 위해 사용한 메서드
+                CallLog.Calls.CONTENT_URI,
+                null,
+                phoneNumber + "=?",
+                arrayOf(phoneNumber),
+                null
+            )
+
+
+            //전체 전화기록을 돌면서 해당하는 값을 가져오기
+            cursor?.use {
+                val numberIndex = it.getColumnIndex(CallLog.Calls.NUMBER)
+                val typeIndex = it.getColumnIndex(CallLog.Calls.TYPE)
+                val dateIndex = it.getColumnIndex(CallLog.Calls.DATE)
+                val durationIndex = it.getColumnIndex(CallLog.Calls.DURATION)
+
+                while (it.moveToNext()) {//전체 전화기록을 돌면서 해당하는 번호의 데이터 callRecord에 추가
+                    //휴대폰 번호
+                    val phNumber = it.getString(numberIndex)
+
+                    //전화 시간
+//                    val callDate = Date(it.getLong(dateIndex)).toString() //GMT 그리니치 표기법 (출력: Wed Apr 24 03:05:22 GMT 2024)
+                    val callDate = Date(it.getLong(dateIndex))
+                    val simpleCallDate = SimpleDateFormat("MM월dd일 HH시mm분", Locale.getDefault())
+                    val newCallDate = simpleCallDate.format(callDate)
+
+                    //통화시간
+                    val callDuration = Date(it.getLong(durationIndex))
+                    val simpleCallDuration = SimpleDateFormat("HH:mm", Locale.getDefault())
+                    val newCallDuration = simpleCallDuration.format(callDuration)
+
+                    //전화 타입
+                    val callType = when (it.getInt(typeIndex)) {
+                        CallLog.Calls.INCOMING_TYPE -> "수신"
+                        CallLog.Calls.OUTGOING_TYPE -> "발신"
+                        CallLog.Calls.MISSED_TYPE -> "부재중"
+                        else -> "" //todo 뭐로하지
+                    }
+
+                    val callRecord = CallInformation(phNumber,callType, newCallDate, newCallDuration)
+                    callRecords.add(callRecord)
+                }
+            }
         }
+        return callRecords //반환
     }
+
 
 }
---
+++ app/src/main/java/com/example/contactapp/presentation/call_log/CallLogAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.contactapp.presentation.call_log
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contactapp.R
+import com.example.contactapp.data.CallInformation
+
+class CallLogAdapter(var callLog: List<CallInformation>) :
+    RecyclerView.Adapter<CallLogAdapter.CallLogViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CallLogViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.call_log_item, parent, false)
+        return CallLogViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CallLogViewHolder, position: Int) {
+        val callLog = callLog[position]
+        holder.bind(callLog)
+    }
+
+    override fun getItemCount(): Int {
+        return callLog.size
+    }
+
+    class CallLogViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val callTypeTextView: TextView = itemView.findViewById(R.id.tvCallType)
+        private val callDateTextView: TextView = itemView.findViewById(R.id.tvCallDate)
+        private val callDurationTextView: TextView = itemView.findViewById(R.id.tvCallDuration)
+
+        fun bind(callLog: CallInformation) {
+            callTypeTextView.text = callLog.callType
+            callDateTextView.text = callLog.callDate
+            callDurationTextView.text = callLog.callDuration
+        }
+    }
+}
