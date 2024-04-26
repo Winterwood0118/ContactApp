45eb78fc87313d82275ab32f849090ff7d0e77ef
WinterWood0118 <dltjdals125@gmail.com>
2024-04-26 11:27:17 +0900

    TapBar 텍스트 가지고 오는 방식 변경

--- app/src/main/java/com/example/contactapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/contactapp/presentation/MainActivity.kt
@@ -31,10 +31,8 @@ class MainActivity : AppCompatActivity() {
         // tab layout 과 view pager2 연동하는 코드입니다.
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             when (position) {
-//                0 -> tab.text = R.string.tab_left.toString() // 전부 대문자로 하는거 맞나요?
-//                1 -> tab.text = R.string.tab_right.toString() // string 데이터 이렇게 가져오는게 맞나요?
-                0 -> tab.text = "CONTACT LIST" // 값이 제대로 안가져와서 일단 이렇게 만들었어요.
-                1 -> tab.text = "MY PAGE"
+                0 -> tab.setText(R.string.tab_left)
+                1 -> tab.setText(R.string.tab_right)
             }
         }.attach()
 
