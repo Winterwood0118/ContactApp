9462ed12575289f12bbb5854e5e51e5fd8dee12a
Combro-Kim <rlawog@nate.com>
2024-04-26 10:23:35 +0900

    forPull

--- app/src/main/java/com/example/contactapp/presentation/AddContact.kt
+++ app/src/main/java/com/example/contactapp/presentation/AddContact.kt
@@ -5,7 +5,9 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Point
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -13,16 +15,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.graphics.drawable.toBitmap
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.DialogFragment
+import com.example.contactapp.R
 import com.example.contactapp.data.ContactInformation
 import com.example.contactapp.data.DataSource
 import com.example.contactapp.databinding.CustomDialogBinding
 import com.example.contactapp.function.setBitmapProfile
 
 class AddContact(private val position: Int) : DialogFragment() {
+    private var selectedUri: Uri? = null
+    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        if (uri != null) { //이미지를 선택할 경우
+            selectedUri = uri
+            //uri -> bitMap으로 변경
+//            BitmapFactory.decodeStream(selectedUri)
+            binding.ivUser.setBitmapProfile()
+        } else {
+            Log.d("PhotoPicker", "No media selected") //todo 이미지 선택하지 않을 경우(알림추가?)
+        }
+    }
 
     interface OnContactAddedListener {
         fun onContactAdded(contactInfo: ContactInformation)
@@ -116,6 +132,15 @@ class AddContact(private val position: Int) : DialogFragment() {
             dismiss()
         }
 
+        //todo 이미지 변경
+        binding.ivEdit.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+        binding.ivDelete.setOnClickListener {
+            selectedUri = null
+            binding.ivUser.setImageResource(R.drawable.ic_default_user)
+        }
+
 
         return binding.root
     }
@@ -157,4 +182,5 @@ class AddContact(private val position: Int) : DialogFragment() {
             window?.setLayout(x, y)
         }
     }
+
 }
